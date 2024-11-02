c3ad1f86c0b1f1119134fa84ae5a36ab7066f76c
abc <abhaygupta.hsj@gmail.com>
2024-11-02 18:25:02 +0530

    Javascript and css works nice!

--- script.js
+++ script.js
@@ -2,22 +2,25 @@ let currentScene = 0;
 const scenes = document.querySelectorAll('.scene');
 
 function showScene(index) {
+    // Hide current scene
     scenes[currentScene].classList.remove('active');
     scenes[currentScene].classList.add('hidden');
 
+    // Update current scene index
     currentScene = index;
 
+    // Show new scene
     scenes[currentScene].classList.remove('hidden');
     scenes[currentScene].classList.add('active');
 }
 
 function nextScene() {
-    if (currentScene < scenes.length - 1){
-        showScene(currentScene+1);
-    }
-    else{
-        console.log("the End!") // will change this later
+    if (currentScene < scenes.length - 1) {
+        showScene(currentScene + 1);
+    } else {
+        console.log("The End!"); // Can replace with an ending scene or action
     }
 }
 
-document.addEventListener('click', nextScene)
+// Trigger the next scene on click
+document.addEventListener('click', nextScene);
