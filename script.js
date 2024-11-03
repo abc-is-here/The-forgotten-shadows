9db6d275f9606e2e457d1c49d855326edbe48d50
abc <abhaygupta.hsj@gmail.com>
2024-11-03 16:54:07 +0530

    First scene is almost done!

--- script.js
+++ script.js
@@ -2,14 +2,10 @@ let currentScene = 0;
 const scenes = document.querySelectorAll('.scene');
 
 function showScene(index) {
-    // Hide current scene
     scenes[currentScene].classList.remove('active');
     scenes[currentScene].classList.add('hidden');
-
-    // Update current scene index
     currentScene = index;
 
-    // Show new scene
     scenes[currentScene].classList.remove('hidden');
     scenes[currentScene].classList.add('active');
 }
@@ -18,9 +14,8 @@ function nextScene() {
     if (currentScene < scenes.length - 1) {
         showScene(currentScene + 1);
     } else {
-        console.log("The End!"); // Can replace with an ending scene or action
+        console.log("The End!");
     }
 }
 
-// Trigger the next scene on click
 document.addEventListener('click', nextScene);
